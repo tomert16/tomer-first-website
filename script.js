390664ac2d47eb295c11368cafb2d5e5b32af9a9
tomer <tomertal52@yahoo.com>
2022-11-10 14:49:46 -0500

    Deleted old JS content

--- script.js
+++
@@ -1,7 +0,0 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
-
-const p = document.createElement("p");
-p.textContent = "This is a great way to practice web development skills. Although it may seem overwhelming, take it step by step and everything will be fine. There is this quote: 'Look at the positive things in the process, don't get disappointed from how long it takes, small steps lead to the greatest results' "
-document.querySelector("body").appendChild(p);
